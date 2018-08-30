5c126a335b5abc459519a2150cf16889578f2fb7
Apoorv Yadav <32174554+apoorvyadav1111@users.noreply.github.com>
2018-08-30 15:17:40 +0530

    Add files via upload

---
+++ stuhome.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apoorv yadav
+ * Date: 20-05-2018
+ * Time: 19:11
+ */
+session_start();
+echo "welcome ";
+echo $_SESSION['fname'];
+?>
+<a href="internships.php">View Internships</a><br>
+
+
+<a href="applied.php">View Applied Internships</a><br>
+
+<a href="logout.php">Logout?</a><br>
